a952e27fb34d84823ef5637157192fa53b0250be
Dit <145139482+Elite011@users.noreply.github.com>
2024-07-09 19:36:10 +0530

    deploy vercel app

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 from tmdb import get_movies_by_genre, get_movie_details
+import os
 
 app = Flask(__name__)
 
@@ -15,5 +16,9 @@ def recommend():
     movies = get_movies_by_genre(genre_id, language)
     return jsonify(movies)
 
-if __name__ == '__main__':
+# The below code is required for Vercel to detect the app
+from werkzeug.middleware.proxy_fix import ProxyFix
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
+
+if __name__ == "__main__":
     app.run(debug=True)
